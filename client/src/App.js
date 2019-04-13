7864156e391a3238995cb161214ad4eb29138756
pi0pi0 <ailblue@nate.com>
2019-04-14 00:00:07 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -19,35 +19,25 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  { 
-    'id' : 1,
-    'image' : 'https://placeimg.com/64/64/1',
-    'name' : "hyejin",
-    'gender' : "여자",
-    'age' : "26",
-    'birthday' : "940218"
-  },
-  {
-    'id' : 2,
-    'image' : 'https://placeimg.com/64/64/2',
-    'name' : "lee",
-    'gender' : "남자",
-    'age' : "26",
-    'birthday' : "930218"
-    },
-  {
-    'id' : 3,
-    'image' : 'https://placeimg.com/64/64/3',
-    'name' : "kim",
-    'gender' : "male",
-    'age' : "26",
-    'birthday' : "960218"
-  }
-]
 
 class App extends Component {
- 
+
+  state = {
+    customers :""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () =>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;    
     return (
@@ -65,12 +55,13 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map( c => {return (<Customer key = {c.id} id={c.id}  image={c.image}  name={c.name}  gender={c.gender} age={c.age}  birthday={c.birthday} /> )})}
+            {this.state.customers ? this.state.customers.map( c => {return (<Customer key = {c.id} id={c.id}  image={c.image} name={c.name}  gender={c.gender} age={c.age}  birthday={c.birthday} /> )}) : ""}
           </TableBody>
         </Table>
       </Paper>   
     );
   }
+
 }
 
 export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -6,8 +6,34 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers',(req,res)=>{
+    res.send([
+        { 
+            'id' : 1,
+            'image' : 'https://placeimg.com/64/64/1',
+            'name' : "hyejin",
+            'gender' : "여자",
+            'age' : "26",
+            'birthday' : "940218"
+          },
+          {
+            'id' : 2,
+            'image' : 'https://placeimg.com/64/64/2',
+            'name' : "lee",
+            'gender' : "남자",
+            'age' : "26",
+            'birthday' : "930218"
+            },
+          {
+            'id' : 3,
+            'image' : 'https://placeimg.com/64/64/3',
+            'name' : "kim",
+            'gender' : "male",
+            'age' : "26",
+            'birthday' : "960218"
+          }
+    ]);
 });
+
 
 app.listen(port, () => console.log(`Listening on port ${port}` ));
